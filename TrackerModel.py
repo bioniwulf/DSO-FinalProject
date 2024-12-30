12a951df69b131346c7240112a939050b2a215e8
Anton Tolstonogov <anton_tolstonogov@cashalot.co>
2024-12-31 07:16:42 +1000

    refactor(python): Refector code, add function descriptions
    undefined

--- TrackerModel.py
+++ TrackerModel.py
@@ -2,6 +2,7 @@ from casadi import *
 import numpy as np
 
 class TrackerModel:
+    """MPC Tracker class to facilitate modelling."""
     def __init__(self, label: str, prediction_horizon: int):
         self._label = label
         self._horizon = prediction_horizon
@@ -41,11 +42,33 @@ class TrackerModel:
 
         # Define the symbolic state predictions for all prediction steps + 1
         self._sym_prediction_state = SX.sym(self._label + 'Prediction', st_size, (self._horizon + 1))
+        
+        # Storage for the states throughout the simulation (python list of vectors)
+        self._storage_states = []
+        
+        # Storage for predictions (python list of 2D matrix) throughout the simulation
+        self._storage_predictions = []
+        
+        # Storage for control actions (2D matrix: (cumulative) x Control Vector Size)
+        self._storage_control = []
 
-    def create_objective(self, weight_state: np.array, weight_control: np.array):
+    def create_objective(self, weight_state: np.array, weight_control: np.array) -> SX:
+        """
+        Create objective function for MPC (only for the tracker itself).
+
+        Args:
+        ----
+            weight_state (np.array): weights for state (Diag matrix 3x3).
+            weight_control (np.array): weights for control (Diag matrix 2x2).
+
+        Returns:
+        -------
+            SX: Objective function (Casadi).
+
+        """
         # Define objective function
         objective_fn = 0
-        
+
         st_size   = self._sym_state_vector.size()[0]
         ctrl_size = self._sym_control_vector.size()[0]
 
@@ -69,7 +92,19 @@ class TrackerModel:
                 (control - target_control).T @ weight_control @ (control - target_control)
         return objective_fn
 
-    def create_constraints(self, time_discrete: float):
+    def create_constraints(self, time_discrete: float) -> list[SX]:
+        """
+        Create list of equality symbolic constraints for each state within prediction horizon.
+
+        Args:
+        ----
+            time_discrete (float): time discrete, [s].
+
+        Returns:
+        -------
+            list[SX]: list of symbolic constraints.
+
+        """
         # Temporary define equality constraints as a python list
         constraints_equality = []
         constraints_equality.append(self._sym_prediction_state[:, 0] - self._sym_state_init)
@@ -88,15 +123,36 @@ class TrackerModel:
             constraints_equality.append(state_next - state_next_euler)
         return constraints_equality
     
-    def get_global_limits(self):
+    def get_global_limits(self) -> list:
+        """
+        Create list list of global limits for equality constraints.
+
+        Returns:
+        -------
+            list: list of global limits.
+
+        """
         st_size = self._sym_state_vector.size()[0]
-        
+
         lower_bounds = np.zeros((st_size * (self._horizon + 1), 1))
         upper_bounds = np.zeros((st_size * (self._horizon + 1), 1))
-         
+
         return (lower_bounds, upper_bounds)
 
-    def get_state_limits(self, linear_velocity: list, angular_velocity: list):
+    def get_state_limits(self, linear_velocity: list, angular_velocity: list) -> list[np.array]:
+        """
+        Create list of constraints on state and control vectors (lower and upper bounds).
+
+        Args:
+        ----
+            linear_velocity (list): linear velocity constraints, (min, max), [m/s].
+            angular_velocity (list): angular velocity constraints, (min, max), [rad/s].
+
+        Returns:
+        -------
+            list[np.array]: list of lower and upper constraints.
+
+        """
         st_size   = self._sym_state_vector.size()[0]
         ctrl_size = self._sym_control_vector.size()[0]
 
@@ -112,77 +168,167 @@ class TrackerModel:
         lower_bounds[st_size * (self._horizon + 1) + 0:st_size * (self._horizon + 1) + ctrl_size * self._horizon:2] = np.min(linear_velocity)
         lower_bounds[st_size * (self._horizon + 1) + 1:st_size * (self._horizon + 1) + ctrl_size * self._horizon:2] = np.min(angular_velocity)
 
-        # Set the upper bound for the optimized vector (both state and control vector) symmetricaly
+        # Set the upper bound for the optimized vector (both state and control vector) symmetrically
         upper_bounds = np.zeros((ctrl_size * self._horizon + st_size * (self._horizon + 1), 1))
         upper_bounds[0:(3 * self._horizon + 1) + 0:3] = np.inf
         upper_bounds[1:(3 * self._horizon + 1) + 1:3] = np.inf
         upper_bounds[2:(3 * self._horizon + 1) + 2:3] = np.inf
         upper_bounds[st_size * (self._horizon + 1) + 0:st_size * (self._horizon + 1) + ctrl_size * self._horizon:2] = np.max(linear_velocity)
         upper_bounds[st_size * (self._horizon + 1) + 1:st_size * (self._horizon + 1) + ctrl_size * self._horizon:2] = np.max(angular_velocity)
-        
+
         return (lower_bounds, upper_bounds)
 
-    def get_state_prediction(self, step=None):
+    def get_state_prediction(self, step: int = None) -> np.array:
+        """
+        Get symbolic state predictions within the prediction horizon.
+
+        Args:
+        ----
+            step (int, optional): particular step of the prediction horizon. Defaults to None.
+
+        Returns:
+        -------
+            np.array: symbolic state predictions.
+        """
         if step is None:
             return self._sym_prediction_state
         else:
             return self._sym_prediction_state[:, step]
 
-    def init_storages(self):
-        # Storage for the states throughout the simulation (python list of vectors)
-        self._storage_states = []
-        # Storage for predictions (python list of 2D matrix) throughout the simulation
-        self._storage_predictions = []
-        # Storage for control actions (2D matrix: (cumulative) x Control Vector Size)
-        self._storage_control = []
+    @property
+    def state_vector(self) -> DM:
+        """
+        Get access to the symbolic state vector of the tracker.
 
-    def convert_storages(self):
-        # Convert list of matrix to 3D np matrix and put list into the 3rd dimension
-        self._storage_predictions = np.stack(self._storage_predictions, axis=2)
+        Returns:
+        -------
+            DM: symbolic state vector of the tracker.
 
-        # Concatinate all data along the second axis (because state is the column-vector)
-        self._storage_states = np.concatenate(self._storage_states, axis=1)
-
-        # Concatinate all data along the first axis (because control is the row-vector)
-        self._storage_control = np.concatenate(self._storage_control, axis=0)
-    
-    @property
-    def storage_states(self):
-        return self._storage_states
-    
-    @property
-    def storage_predictions(self):
-        return self._storage_predictions
-    
-    @property
-    def storage_target_states(self):
-        return self._storage_target_states
-    
-    @property
-    def storage_control(self):
-        return self._storage_control
-    
-
-    @property
-    def state_init(self):
-        return self._sym_state_init
-
-    @property
-    def state_reference(self):
-        return self._sym_reference
-
-    @property
-    def state_vector(self):
+        """
         return self._sym_state_vector
 
     @property
-    def control_vector(self):
+    def control_vector(self) -> DM:
+        """
+        Get access to the symbolic control vector of the tracker.
+
+        Returns:
+        -------
+            DM: symbolic control vector of the tracker.
+
+        """
         return self._sym_control_vector
 
     @property
-    def control_prediction(self):
+    def control_prediction(self) -> SX:
+        """
+        Get access to the symbolic control predictions within the prediction horizon.
+
+        Returns:
+        -------
+            SX: symbolic control predictions.
+
+        """
         return self._sym_prediction_control
 
     @property
-    def kin_function(self):
+    def kin_function(self) -> Function:
+        """
+        Get access to the symbolic kinematic function of the tracker.
+
+        Returns:
+        -------
+            Function: symbolic kinematic function.
+
+        """
         return self._sym_kin_function
+
+    @property
+    def state_init(self) -> SX:
+        """
+        Get access to the symbolic initial state of the tracker.
+
+        Returns:
+        -------
+            SX: Symbolic initial state of the tracker.
+
+        """
+        return self._sym_state_init
+
+    @property
+    def state_reference(self) -> SX:
+        """
+        Get access to the symbolic reference states of the tracker within the prediction horizon.
+
+        Returns:
+        -------
+            SX: symbolic reference states.
+
+        """
+        return self._sym_reference
+
+    def storage_state(self, state: np.array) -> None:
+        """
+        Add state to the internal storage.
+
+        Args:
+        ----
+            state (np.array): tracker state.
+
+        """
+        self._storage_states.append(state)
+
+    def get_state_storage(self) -> np.array:
+        """
+        Return accumulated states as np matrix.
+
+        Returns:
+        -------
+            np.array: storage of states.
+
+        """
+        return np.concatenate(self._storage_states, axis=1)
+
+    def storage_control(self, control: np.array) -> None:
+        """
+        Add control to the internal storage.
+
+        Args:
+        ----
+            control (np.array): tracker control.
+
+        """
+        self._storage_control.append(control)
+    
+    def get_control_storage(self) -> np.array:
+        """
+        Return accumulated controls as np matrix.
+
+        Returns:
+        -------
+            np.array: storage of controls
+
+        """
+        return np.concatenate(self._storage_control, axis=0)
+
+    def storage_predictions(self, predictions: np.array) -> None:
+        """
+        Add predictions to the internal storage.
+
+        Args:
+        ----
+            predictions (np.array): predictions.
+
+        """
+        self._storage_predictions.append(predictions)
+    
+    def get_predictions_storage(self) -> np.array:
+        """
+        Return accumulated predictions as np matrix.
+
+        Returns:
+        -------
+            np.array: storage of predictions.
+
+        """
+        return np.stack(self._storage_predictions, axis=2)
